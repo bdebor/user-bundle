9a28245f53c1542e3b1124f9f4afe6c2d4ab8ce5
Benoit Debordeaux <b.debordeaux2@gmail.com>
2017-02-07 13:20:14 +0100

    User.php, $roles is a string

---
+++ src/AppBundle/DataFixtures/ORM/LoadData.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use BD\UserBundle\Entity\User;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class LoadData implements FixtureInterface, ContainerAwareInterface
+{
+	/**
+	 * @var ContainerInterface
+	 */
+	private $container;
+
+	public function setContainer(ContainerInterface $container = null)
+	{
+		$this->container = $container;
+	}
+
+	public function load(ObjectManager $manager)
+	{
+		$user1 = new User();
+		$user1->setEmail('admin@pp.pp');
+		$user1->setUsername('admin');
+		$user1->setIsActive(true);
+		$user1->addRole('ROLE_ADMIN');
+		$token = $this->generateUniqId();
+		$user1->setToken($token);
+		$encoder = $this->container->get('security.password_encoder');
+		$password = $encoder->encodePassword($user1, 'ppppp');
+		$user1->setPassword($password);
+		$createdAt = new \DateTime('now');
+		$user1->setCreatedAt($createdAt);
+		$manager->persist($user1);
+
+		$user2 = new User();
+		$user2->setEmail('user@pp.pp');
+		$user2->setUsername('user');
+		$user2->setIsActive(true);
+		$user2->addRole('ROLE_USER');
+		$token = $this->generateUniqId();
+		$user2->setToken($token);
+		$encoder = $this->container->get('security.password_encoder');
+		$password = $encoder->encodePassword($user2, 'ppppp');
+		$user2->setPassword($password);
+		$createdAt = new \DateTime('now');
+		$user2->setCreatedAt($createdAt);
+		$manager->persist($user2);
+
+		$manager->flush();
+	}
+
+	private function generateUniqId() {
+		$result = bin2hex(openssl_random_pseudo_bytes(16));
+		return $result;
+	}
+}
---
+++ src/BD/UserBundle/Entity/User.php
@@ -0,0 +1,245 @@
+<?php
+
+namespace BD\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="BD\UserBundle\Repository\UserRepository")
+ */
+class User implements UserInterface
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=25, unique=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=60, unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(name="is_active", type="boolean")
+     */
+    private $isActive;
+
+    /**
+     * @ORM\Column(name="token", type="string", length=32)
+     */
+    private $token;
+
+    /**
+     * @ORM\Column(name="roles", type="string") // json_encode($array), length=255 is enough
+     */
+    private $roles;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string 
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set isActive
+     *
+     * @param boolean $isActive
+     * @return User
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    /**
+     * Get isActive
+     *
+     * @return boolean 
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+
+    /**
+     * Set token
+     *
+     * @param string $token
+     * @return User
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    /**
+     * Get token
+     *
+     * @return string 
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    public function getRoles()
+    {
+        return json_decode($this->roles);
+    }
+
+    public function addRole($role)
+    {
+        $roles = json_decode($this->roles);
+        $roles[] = $role;
+        $this->roles = json_encode($roles);
+
+        return $this;
+    }
+
+    public function removeRole($role)
+    {
+        $roles = json_decode($this->roles);
+        $key = array_search($role, $roles, true);
+
+        if ($key === false) {
+            return false;
+        }
+
+        unset($this->$roles[$key]);
+        $this->roles = json_encode($roles);
+
+        return true;
+    }
+
+    public function eraseCredentials() {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    public function getSalt() {
+        // TODO: Implement getSalt() method.
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return User
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+}
