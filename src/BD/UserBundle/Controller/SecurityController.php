3709e3c96995a800cb67283ef4e573967fb8e903
Benoit Debordeaux <b.debordeaux2@gmail.com>
2017-02-07 18:58:01 +0100

    login form

---
+++ src/BD/UserBundle/Controller/SecurityController.php
@@ -0,0 +1,31 @@
+<?php
+
+// http://symfony.com/doc/current/security/form_login_setup.html
+
+namespace BD\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('@BDUser/security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
+}
